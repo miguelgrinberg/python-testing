0f68b0fa47f7bcaac9e0cf3f3db1d7a38f107617
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-03-14 19:40:36 +0000

    game of life

---
+++ life.py
@@ -0,0 +1,84 @@
+class CellList:
+    def __init__(self):
+        self.cells = {}
+
+    def has(self, x, y):
+        return x in self.cells.get(y, [])
+
+    def set(self, x, y, value=True):
+        if value:
+            row = self.cells.setdefault(y, set())
+            if x not in row:
+                row.add(x)
+        else:
+            try:
+                self.cells[y].remove(x)
+            except KeyError:
+                pass
+            else:
+                if not self.cells[y]:
+                    del self.cells[y]
+
+    def __iter__(self):
+        for y in self.cells:
+            for x in self.cells[y]:
+                yield (x, y)
+
+
+class Life:
+    def __init__(self):
+        self.alive = CellList()
+
+    def load(self, filename, x, y):
+        with open(filename, "rt") as f:
+            for line in f.readlines():
+                i = line.find('*')
+                while i != -1:
+                    self.alive.set(x + i, y, True)
+                    i = line.find('*', i + 1)
+                y += 1
+
+    def bounding_box(self):
+        min_x = min_y = max_x = max_y = None
+        for cell in self.alive:
+            x = cell[0]
+            y = cell[1]
+            if min_x is None or x < min_x:
+                min_x = x
+            if min_y is None or y < min_y:
+                min_y = y
+            if max_x is None or x > max_x:
+                max_x = x
+            if max_y is None or y > max_y:
+                max_y = y
+        return (min_x or 0, min_y or 0, max_x or 0, max_y or 0)
+
+    def advance(self):
+        new_alive = CellList()
+        for cell in self.alive:
+            x = cell[0]
+            y = cell[1]
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if self.advance_cell(x + i, y + j):
+                        new_alive.set(x + i, y + j, True)
+        self.alive = new_alive
+
+    def advance_cell(self, x, y):
+        neighbors = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i != 0 or j != 0:
+                    neighbors += 1 if self.alive.has(x + i, y + j) else 0
+
+        if self.alive.has(x, y):
+            if neighbors < 2:
+                return False
+            elif neighbors > 3:
+                return False
+            else:
+                return True
+        elif neighbors == 3:
+            return True
+        else:
+            return False
---
+++ life_gui.py
@@ -0,0 +1,92 @@
+import sys
+import pygame
+from life import Life
+
+SCREEN_SIZE = 500
+FPS = 5
+
+life = Life()
+
+
+def initialize_game(pattern_file=None, x=None, y=None):
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
+
+    if pattern_file:
+        life.load(pattern_file, None or 0, None or 0)
+
+    return screen
+
+
+def center(scale):
+    cell_count = SCREEN_SIZE // scale
+    min_x, min_y, max_x, max_y = life.bounding_box()
+
+    basex = (cell_count - (max_x - min_x + 1)) // 2 - min_x
+    basey = (cell_count - (max_y - min_y + 1)) // 2 - min_y
+    return basex, basey
+
+
+def game_loop(screen):
+    running = True
+    scale = 20
+    basex, basey = center(scale)
+    interval = 1000 // FPS
+
+    while running:
+        start_time = pygame.time.get_ticks()
+
+        screen.fill((255, 255, 255))
+        for i in range(0, SCREEN_SIZE, scale):
+            pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, SCREEN_SIZE))
+            pygame.draw.line(screen, (0, 0, 0), (0, i), (SCREEN_SIZE, i))
+        for cell in life.alive:
+            x = cell[0]
+            y = cell[1]
+            pygame.draw.rect(screen, (80, 80, 192),
+                             ((basex + x) * scale + 2, (basey + y) * scale + 2,
+                              scale - 3, scale - 3))
+
+        pygame.display.flip()
+        life.advance()
+
+        wait_time = 1
+        while wait_time > 0:
+            event = pygame.event.wait(timeout=wait_time)
+            while event:
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                    elif event.key == pygame.K_LEFT:
+                        basex += 2
+                    elif event.key == pygame.K_RIGHT:
+                        basex -= 2
+                    elif event.key == pygame.K_UP:
+                        basey += 2
+                    elif event.key == pygame.K_DOWN:
+                        basey -= 2
+                    elif event.unicode == '+':
+                        if scale < 50:
+                            scale += 5
+                    elif event.unicode == '-':
+                        if scale > 10:
+                            scale -= 5
+                    elif event.unicode == 'c':
+                        basex, basey = center(scale)
+                    break
+                event = pygame.event.poll()
+            if event:
+                break
+
+            current_time = pygame.time.get_ticks()
+            wait_time = interval - (current_time - start_time)
+
+
+if __name__ == '__main__':
+    pattern_file = sys.argv[1] if len(sys.argv) > 1 else None
+    screen = initialize_game(pattern_file)
+    game_loop(screen)
+    pygame.quit()
